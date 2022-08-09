9108ca2408e52edf5c287b397eb99a7102eee1f7
Jarrett Lirette <jarrettlirette@gmail.com>
2022-08-09 14:24:54 -0400

    Added index.html and script.js files

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+var message = "in global";
+console.log("global: message = " + message);
+
+//var a = function () {
+//    var message = "inside a";
+//    console.log("a: message = " + message);
+//    b();
+//}
+
+//var b = function () {
+//    console.log("b: message + " + message);
+//}
+
